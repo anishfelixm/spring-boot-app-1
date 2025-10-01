42763c80da74eff1a579e79e0df8bc3a3bd116db
Anish Felix Mathias <anishfelixm.cs20@rvce.edu.in>
2025-10-01 11:51:57 +0530

    feat: Project Built and runs on localhost:8080. Project is a simple server

---
+++ src/main/java/org/example/controller/Controller.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World";
+    }
+}
